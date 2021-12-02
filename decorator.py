1606a30236e2d377b5d5113e35e72032c85e83cc
Taras Tsoin <tsoint@mail.ru>
2021-12-02 17:07:25 +0300

    x

--- decorator.py
+++ decorator.py
@@ -1,19 +1,22 @@
 import datetime
 
-def make_logo(old_function):
-  def new_function(*args, p = 'logo/logo.txt'):
-    result = old_function(*args)
-    with open (p, 'a') as file:      
-      file.write(f'вызвана функция {old_function.__name__} с аргументом {args}, получен результат {result}  {datetime.datetime.today()}\n')
-    return result
-  return new_function
+def parametrized_logo(path):
+  def make_logo(old_function):
+    def new_function(*args, p = path):
+      result = old_function(*args)
+      with open (p, 'a') as file:      
+        file.write(f'вызвана функция {old_function.__name__} с аргументом {args}, получен результат {result}  {datetime.datetime.today()}\n')
+      return result
+    return new_function
+  return make_logo
+
 
 import requests
 
 hero_list = ["Hulk", "Thanos", "Captain America"]
 intel = {}
 name_hero = {}
-@make_logo
+@parametrized_logo(path='logo/logo.txt')
 def request_(name):
   url = "https://superheroapi.com/api/2619421814940190/search/"
   response = requests.get(url+name, timeout = 5)
@@ -28,4 +31,3 @@ max_int = max(intel.values(), key=lambda i: int(i))
 smart_hero = name_hero[max_int]
 
 print(smart_hero, max_int)
-    
